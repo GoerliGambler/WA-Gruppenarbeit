6a9213095f8c1811090c22dc9834b9463a484b92
PatrickHeuer <patrick-heuer@gmx.de>
2021-01-31 10:28:45 +0100

    Update Datensatz.R

--- Datensatz.R
+++ Datensatz.R
@@ -10,4 +10,20 @@ alter = rnorm( n=100 , mean=25 , sd=2 )
 fächer = c("Statistik", "Data Science", "Mathe", "Informatik")
 studienfach = sample(fächer, 100, replace = TRUE, prob = c(0.3,0.3,0.15,0.25))
 
-datensatz = data.frame( id, alter, studienfach)
+#Variable "Interesse an Programmieren"
+
+programmieren = c()
+for (i in 1:100){
+  if(studienfach[i] == "Statistik" | studienfach[i] == "Mathe"){
+    programmieren[i] = sample(1:3 ,1, prob = c(0.25 , 0.5, 0.25))
+  }
+  if(studienfach[i] == "Data Science"){
+    programmieren[i] = sample(3:5, 1, prob = c(0.25 , 0.5, 0.25))
+  }
+  if(studienfach[i] == "Informatik"){
+    programmieren[i] = sample(5:7, 1, prob = c(0.25 , 0.5, 0.25))
+  }
+}
+
+datensatz = data.frame( id, alter, studienfach, programmieren)
+datensatz
