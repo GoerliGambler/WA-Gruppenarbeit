a2cff9148d7be34288fcc8c73c5be2b88b218972
jordiw98 <57448770+jordiw98@users.noreply.github.com>
2021-01-31 10:32:36 +0100

    Update Datensatz.R

--- Datensatz.R
+++ Datensatz.R
@@ -7,7 +7,25 @@ id = 1:100
 alter = rnorm( n=100 , mean=25 , sd=2 )
 
 #Variable Studienfach
-f‰cher = c("Statistik", "Data Science", "Mathe", "Informatik")
-studienfach = sample(f‰cher, 100, replace = TRUE, prob = c(0.3,0.3,0.15,0.25))
 
-datensatz = data.frame( id, alter, studienfach)
+f√§cher = c("Statistik", "Data Science", "Mathe", "Informatik")
+studienfach = sample(f√§cher, 100, replace = TRUE, prob = c(0.3,0.3,0.15,0.25))
+
+# Interesse Mathe
+
+mathe = function( x ){
+  res = c()
+  for( i in 1:length(x)){
+    if( studienfach[i] == "Statistik"){ res[i] = sample(c(2:5),1)}
+    if( studienfach[i] == "Data Science"){ res[i] = sample(c(3:5),1)}
+    if( studienfach[i] == "Mathe"){ res[i] = sample(c(1:3),1)}
+    if( studienfach[i] == "Informatik"){ res[i] = sample(c(3:7),1)}
+  }
+  return( res )
+}
+
+interesseMathe = mathe( c(1:100) )
+
+# Datensatz
+
+datensatz = data.frame( id, alter, studienfach, interesseMathe)
