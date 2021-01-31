06d523170ef5f9bb17351492d0ba0daa03edf5b1
jordiw98 <57448770+jordiw98@users.noreply.github.com>
2021-01-31 10:45:32 +0100

    Update Datensatz.R

--- Datensatz.R
+++ Datensatz.R
@@ -1,4 +1,4 @@
-#Datensatz
+Datensatz
 
 id = 1:100
 
@@ -7,9 +7,25 @@ id = 1:100
 alter = rnorm( n=100 , mean=25 , sd=2 )
 
 #Variable Studienfach
-
 fächer = c("Statistik", "Data Science", "Mathe", "Informatik")
 studienfach = sample(fächer, 100, replace = TRUE, prob = c(0.3,0.3,0.15,0.25))
+
+#Variable "Interesse an Programmieren"
+
+programmieren = c()
+for (i in 1:100){
+  if(studienfach[i] == "Statistik" | studienfach[i] == "Mathe"){
+    programmieren[i] = sample(1:3 ,1, prob = c(0.25 , 0.5, 0.25))
+  }
+  if(studienfach[i] == "Data Science"){
+    programmieren[i] = sample(3:5, 1, prob = c(0.25 , 0.5, 0.25))
+  }
+  if(studienfach[i] == "Informatik"){
+    programmieren[i] = sample(5:7, 1, prob = c(0.25 , 0.5, 0.25))
+  }
+}
+
+datensatz = data.frame( id, alter, studienfach, programmieren)
 
 # Interesse Mathe
 
@@ -26,21 +42,4 @@ mathe = function( x ){
 
 interesseMathe = mathe( c(1:100) )
 
-# Variable "Interesse an Programmieren"
-
-programmieren = c()
-for (i in 1:100){
-  if(studienfach[i] == "Statistik" | studienfach[i] == "Mathe"){
-    programmieren[i] = sample(1:3 ,1, prob = c(0.25 , 0.5, 0.25))
-  }
-  if(studienfach[i] == "Data Science"){
-    programmieren[i] = sample(3:5, 1, prob = c(0.25 , 0.5, 0.25))
-  }
-  if(studienfach[i] == "Informatik"){
-    programmieren[i] = sample(5:7, 1, prob = c(0.25 , 0.5, 0.25))
-  }
-}
-
-# Datensatz
-
-datensatz = data.frame( id, alter, studienfach, interesseMathe, programmieren)
+datensatz = data.frame( id, alter, studienfach, programmieren, interesseMathe)
