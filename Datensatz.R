84338a47333db6424184dd55a960a38f2e5ba9f1
PatrickHeuer <74727152+PatrickHeuer@users.noreply.github.com>
2021-01-31 10:17:04 +0100

    Add files via upload

--- Datensatz.R
+++ Datensatz.R
@@ -6,4 +6,8 @@ id = 1:100
 
 alter = rnorm( n=100 , mean=25 , sd=2 )
 
-datensatz = data.frame( id, alter)
+#Variable Studienfach
+fächer = c("Statistik", "Data Science", "Mathe", "Informatik")
+studienfach = sample(fächer, 100, replace = TRUE, prob = c(0.3,0.3,0.15,0.25))
+
+datensatz = data.frame( id, alter, studienfach)
