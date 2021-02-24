9b87e68df4b6e2d6f5aa12126a85dde798ced0f0
StudiumAcc <76392988+StudiumAcc@users.noreply.github.com>
2021-02-24 16:34:12 +0100

    Update Hilfsfunktionen.R
    Erste Hilfsfunktion Daten_str hinzugefügt

--- Hilfsfunktionen.R
+++ Hilfsfunktionen.R
@@ -1,2 +1,15 @@
 ## Hilfsfunktionen
-# Funktionen die ggf. zu den Hauptfunktionen helfen koennen
+# Funktionen die ggf. von den Hauptfunktionen als Hilfe benutzt werden koennen
+
+# 1. Hilfsfunktion
+# Funktino Daten_str : Gibt allgemeine Daten ueber die Datenstruktur 
+#                      des Objekts aus
+# Eingabe : data - Objekt
+# Ausgabe : Liste mit (1)Struktur (2)Klasse und (3) Datentyp 
+#           des uebergebenen Arguments/Objekts
+Daten_str <- function(data){
+  Liste <- list(Struktur = str(data), 
+                Klasse = class(data),
+                Datentyp = typeof(data))
+  return(Liste)
+}
